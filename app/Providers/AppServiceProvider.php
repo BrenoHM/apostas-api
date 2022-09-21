69322e7cb33dd38dadb01afa19d1f5b2c7f5faa7
brenomol <emaildobrenomol@gmail.com>
2022-09-21 18:06:52 -0300

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (config('app.env') != 'local') {
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
